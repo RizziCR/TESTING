a9cea459ebda7ef6f991126a957729dc64e7456a
RizziCR <cr@rizzotti-tec.de>
2015-06-18 02:47:21 +0200

    remove spring security

--- springfox/src/main/java/de/rizzicr/testing/springfox/Application.java
+++ springfox/src/main/java/de/rizzicr/testing/springfox/Application.java
@@ -1,29 +1,16 @@
 package de.rizzicr.testing.springfox;
 
-import com.google.i18n.phonenumbers.Phonenumber;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration;
-import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.net.URL;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.ZonedDateTime;
 
 /**
  * Created by rizzi on 26.05.15.
@@ -45,8 +32,13 @@ public class Application {
                 .select()
                     .apis(RequestHandlerSelectors.basePackage("de.rizzicr.testing.springfox"))
                     .paths(PathSelectors.any())
-                .build();
+                    .build();
         //@formatter:on
+    }
+
+    @Bean
+    UiConfiguration uiConfig() {
+        return new UiConfiguration("validatorUrl");
     }
 
 }
