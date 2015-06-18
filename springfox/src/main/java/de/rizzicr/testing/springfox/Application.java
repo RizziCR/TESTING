aca668cca987b193fbab7520b89c9f40f28bd3c6
RizziCR <cr@rizzotti-tec.de>
2015-06-18 03:38:36 +0200

    remove ui controller from api default log level to info

--- springfox/src/main/java/de/rizzicr/testing/springfox/Application.java
+++ springfox/src/main/java/de/rizzicr/testing/springfox/Application.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.UiConfiguration;
--- springfox/src/main/java/de/rizzicr/testing/springfox/controller/UiController.java
+++ springfox/src/main/java/de/rizzicr/testing/springfox/controller/UiController.java
@@ -2,11 +2,13 @@ package de.rizzicr.testing.springfox.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.annotations.ApiIgnore;
 
 /**
  * Created by rizzi on 27.05.15.
  */
 @Controller
+@ApiIgnore
 public class UiController {
 
     @RequestMapping(value = "/")
