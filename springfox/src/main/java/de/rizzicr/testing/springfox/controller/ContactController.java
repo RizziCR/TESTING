31678642cd4f458a15569f4765b06043359d3b2c
RizziCR <christoph.rizzotti@gmail.com>
2015-07-03 23:59:49 +0200

    add tests for RequestPart behaviour of entity models

--- springfox/src/main/java/de/rizzicr/testing/springfox/controller/ContactController.java
+++ springfox/src/main/java/de/rizzicr/testing/springfox/controller/ContactController.java
@@ -4,12 +4,13 @@ import de.rizzicr.testing.springfox.domain.Contact;
 import de.rizzicr.testing.springfox.service.ContactService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -19,6 +20,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/contacts/")
 @Api(value = "contacts", description = "API for contact operations")
+@Slf4j
 public class ContactController {
 
     @Autowired
@@ -37,5 +39,27 @@ public class ContactController {
             return contactService.findOne(contactId);
         }
         return Contact.builder().build();
+    }
+
+    @RequestMapping(value = "addContact1", method = RequestMethod.POST)
+    @ApiOperation(value = "Add a new contact")
+    public ResponseEntity<Void> addContact1(Contact contact) {
+        log.debug(contact.toString());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "addContact2", method = RequestMethod.POST)
+    @ApiOperation(value = "Add a new contact")
+    public ResponseEntity<Void> addContact2(@RequestPart Contact contact) {
+        log.debug(contact.toString());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "addContactWithFile", method = RequestMethod.POST)
+    @ApiOperation(value = "Add a new contact with file attachment")
+    public ResponseEntity<Void> addContactWithFile(@RequestPart Contact contact, @RequestPart MultipartFile singleFile) {
+        log.debug(contact.toString());
+        log.debug(singleFile.getName());
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
